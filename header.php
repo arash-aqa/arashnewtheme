c0752f7b8242c74f106e23627ee5da35ac696692
arash <arashaqa@gmail.com>
2021-09-04 06:15:43 +0430

    delet composer

---
+++ header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+	<link rel="icon" href="build/assets/images/Logo-farkoo.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<title>خانه</title>
+	<link rel="stylesheet" href="build/css/homepage-style.css">
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+	<script src="jquery3.5.1.min.js"></script>
+
+</head>
+<body class="animated-gradient" dir="rtl">
+
+<div class="top-header-wrapper">
+    <div class="top-header"><span class="address">آدرس دفتر : تهران ، ونک ، ده ونک ، خیابان شیخ بهایی ، کوچه کولیوند ، خیابان شهید مجید پور ، نبش بن بست آرمین پلاک ۱۰۶ </span><span
+                class="tel"><span class="tel-text">شماره تماس :</span>  02188068352</span></div>
+</div>
+<header>
+
+    </div>
+    <div class="in-header">
+        <div class="logo"><a href="#"><img src="build/assets/images/Logo-farkoo.png" alt=""></a></div>
+        <div id="nav-wrapper" class="nav-wrapper">
+            <div class="nav-icon-wrapper mobile-hamburger">
+                <div id="nav-icon" class="nav-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <nav class="nav">
+                <ul class="menu">
+                    <li class="active"><a href="#">خانه</a></li>
+                    <li><a href="#">فروشگاه</a></li>
+                    <li class="menu-item-has-children"><a href="#">خدمات</a>
+                        <ul class="sub-menu">
+                            <li><a href="#">شیشه</a></li>
+                            <li class="menu-item-has-children"><a href="#">آینه</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">آینه رنگی</a></li>
+                                    <li><a href="#">آینه دکوراتیو</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">پلکسی گلس</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">درباره ی ما</a></li>
+                    <li><a href="#">مقالات</a></li>
+                    <li><a href="#">نمونه کار ها</a></li>
+                    <li><a href="#">ارتباط با ما</a></li>
+                </ul>
+
+            </nav>
+        </div>
+        <div class="search-icon"><a href="#"><span data-icon="&#x55"></span></a></div>
+        <div class="cart-icon"><a href="#"><span data-icon="&#xe015"></span></a></div>
+
+
+</header>
