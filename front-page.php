2df07aa0226c9b4e2e01bbe2fab24dec2edd2c4e
arash <arashaqa@gmail.com>
2021-09-04 15:50:57 +0430

    night edits

---
+++ front-page.php
@@ -0,0 +1,58 @@
+<?php
+    get_header();
+    wp_head();
+?>
+
+
+<div class="hero-wrapper">
+    <div class="hero">
+        <img class="hero-bg" src="build/assets/images/hero-bg.jpg" alt="">
+        <div class="hero-text">با افتخار تولید کننده ی <span class="hero-text-ul">بهترین کیفیت</span> <span
+                    class="hero-text-bold">شیشه و آینه</span> در ایران
+        </div>
+        <div class="hero-main-text">شما لایق بهترین دکوراسیون در محل کار و یا خانه هستید. ما با تضمین بهترین کیفیت در
+            کنار شما ایستاده ایم. با استفاده از شیشه ها و آینه های با کیفیت صادراتی این امکان را به شما میدهیم که از
+            جدیدترین تکولوژی روز دنیا در کنار بالاترین کیفیت، از زندگی خود لذت ببرید.
+            <span class="bold">برای سفارش تماس بگیرید.</span>
+        </div>
+        <div class="hero-button-group">
+            <a class="button blue-button">وبلاگ</a>
+            <a class="button white-button">فروشگاه</a>
+        </div>
+        <div class="hero-sub">
+            <div class="hero-sub-container">
+                <div class="button hero-sub-button"><span data-icon="&#x5a" class="hero-sub-button-icon"></span>نصب در
+                    محل
+                </div>
+                <div class="button hero-sub-button"><span data-icon="&#x6c" class="hero-sub-button-icon"></span>گارانتی
+                    مادام العمر
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="c"></div>
+<section class="posts-wrapper">
+    <div class="pots">
+        <div class="glass-table-top">
+            <div class="glass-table-top__inner">
+                <div class="glass-table-top__name">شیشه رومیزی</div>
+                <div class="glass-table-top__posts">
+
+                </div>
+            </div>
+        </div>
+        <div class="glass-table">
+
+        </div>
+    </div>
+</section>
+<main>
+
+</main>
+<script src="build/js/homepage-style.js"></script>
+<?php
+    get_footer();
+    wp_footer();
+?>
---
+++ sidebar.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+hellp from index
+<?php get_footer(); ?>
